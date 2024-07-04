9f8432c05dcab1bc8be74b84d79d79bce0dae0c6
김서짱 <seojeong1101@gmail.com>
2024-07-04 23:49:50 +0900

    7/5 문제풀이

---
+++ Python_Basic/Q3-6.py
@@ -0,0 +1,17 @@
+n,k = map(int,input().split())
+
+result = 0
+
+while True:
+    # (N == K로 나누어 떨어지는 수)가 될 때까지 1씩 빼기
+    target = (n//k) * k
+    result += (n - target)
+    n = target
+
+    if n < k:
+        break
+    result += 1
+    n //= k
+
+result += (n-1)
+print(result)
