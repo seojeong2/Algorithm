1ac37812480716f1340aac43d0c61d7539c654d4
김서짱 <seojeong1101@gmail.com>
2024-09-19 23:05:10 +0900

    9/19 문제풀이

--- Baekjoon2847.py
+++ Baekjoon2847.py
@@ -9,7 +9,7 @@ for i in range(len(data)-1,0,-1):
         num = data[i-1] - data[i] + 1
         result += num
         data[i-1] -= num
-    elif data[i] == data[i-1]:
+    elif data[i] == data[i-1]: 
         result += 1
         data[i-1] -= 1
     else:
